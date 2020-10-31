f7ca5065f3bb662e635712577a6bb262868d1de8
arrowkato <increntalmodel@gmail.com>
2020-10-31 22:12:16 +0900

    8章の説明を追加

--- ch08/attention_layer.py
+++ ch08/attention_layer.py
@@ -13,7 +13,7 @@ class WeightSum:
     def forward(self, hs, a):
         N, T, H = hs.shape
 
-        ar = a.reshape(N, T, 1)#.repeat(T, axis=1)
+        ar = a.reshape(N, T, 1)  #.repeat(T, axis=1)
         t = hs * ar
         c = np.sum(t, axis=1)
 
@@ -40,7 +40,7 @@ class AttentionWeight:
     def forward(self, hs, h):
         N, T, H = hs.shape
 
-        hr = h.reshape(N, 1, H)#.repeat(T, axis=1)
+        hr = h.reshape(N, 1, H)  #.repeat(T, axis=1)
         t = hs * hr
         s = np.sum(t, axis=2)
         a = self.softmax.forward(s)
@@ -51,10 +51,10 @@ class AttentionWeight:
     def backward(self, da):
         hs, hr = self.cache
         N, T, H = hs.shape
-
+        # dsはsを逆伝播させるときの行列
         ds = self.softmax.backward(da)
         dt = ds.reshape(N, T, 1).repeat(H, axis=2)
-        dhs = dt * hr
+        dhs = dt * hr  # hrの内積を取る意味がわからん
         dhr = dt * hs
         dh = np.sum(dhr, axis=1)
 
@@ -69,12 +69,15 @@ class Attention:
         self.attention_weight = None
 
     def forward(self, hs, h):
+        # 下から順にforwardするので、Attention Weight, Weight Sumの順
         a = self.attention_weight_layer.forward(hs, h)
         out = self.weight_sum_layer.forward(hs, a)
+        # 結果をあとから参照するため
         self.attention_weight = a
         return out
 
     def backward(self, dout):
+        # 上から順にbackwardするので、Weight Sum, Attention Weightの順
         dhs0, da = self.weight_sum_layer.backward(dout)
         dhs1, dh = self.attention_weight_layer.backward(da)
         dhs = dhs0 + dhs1
@@ -88,6 +91,11 @@ class TimeAttention:
         self.attention_weights = None
 
     def forward(self, hs_enc, hs_dec):
+        '''
+        hs_enc: encoderから渡される値-> hs
+        hs_dec: 下(LSTM)から順伝播される値
+        '''
+        # T:seq2seqの単語数 e.g. 5: <eos> I am a cat
         N, T, H = hs_dec.shape
         out = np.empty_like(hs_dec)
         self.layers = []
@@ -95,21 +103,28 @@ class TimeAttention:
 
         for t in range(T):
             layer = Attention()
-            out[:, t, :] = layer.forward(hs_enc, hs_dec[:,t,:])
+            # hs_encはすべてのAttentionレイヤで同じものを使う
+            # hs_decつまり、LSTMからの値は、同じ時間TのLSTMから伝播してくる値を使う
+            out[:, t, :] = layer.forward(hs_enc, hs_dec[:, t, :])
             self.layers.append(layer)
             self.attention_weights.append(layer.attention_weight)
 
+        # Affineレイヤに順伝播させる値(の一部)を返す
         return out
 
     def backward(self, dout):
         N, T, H = dout.shape
         dhs_enc = 0
+        # doutと同じ行と列で、入っている値は適当な行列を作る
         dhs_dec = np.empty_like(dout)
 
         for t in range(T):
             layer = self.layers[t]
+            # 時刻:Tのときの dout からdhs_encとdhs_decの逆伝播を求める　図8-20参照
             dhs, dh = layer.backward(dout[:, t, :])
+            # dhs_enc自体は、T個あるAttentionレイヤすべてから逆伝播した値
             dhs_enc += dhs
-            dhs_dec[:,t,:] = dh
+            # 時刻:Tのときのdoutから逆伝播した値をdhs_decとして受け取る
+            dhs_dec[:, t, :] = dh
 
         return dhs_enc, dhs_dec
--- ch08/attention_seq2seq.py
+++ ch08/attention_seq2seq.py
@@ -7,12 +7,24 @@ from ch08.attention_layer import TimeAttention
 
 
 class AttentionEncoder(Encoder):
+    '''
+    親クラスのEncoderの実装は、ch07/seq2seq.py参照
+    実装の解説は7.3.1節
+    '''
     def forward(self, xs):
+        '''
+        前章の Encoder クラスの forward() メソッドは、
+        LSTM レイヤの最後の隠れ状態ベクトルだけを返しました。
+        それに対して、今回はすべての隠れ状態を返します。
+        '''
         xs = self.embed.forward(xs)
         hs = self.lstm.forward(xs)
-        return hs
+        return hs  # 親クラスだと return hs[:, -1, :]
 
     def backward(self, dhs):
+        '''
+        wae
+        '''
         dout = self.lstm.backward(dhs)
         dout = self.embed.backward(dout)
         return dout
@@ -27,13 +39,15 @@ class AttentionDecoder:
         lstm_Wx = (rn(D, 4 * H) / np.sqrt(D)).astype('f')
         lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
         lstm_b = np.zeros(4 * H).astype('f')
-        affine_W = (rn(2*H, V) / np.sqrt(2*H)).astype('f')
+        affine_W = (rn(2 * H, V) / np.sqrt(2 * H)).astype('f')
         affine_b = np.zeros(V).astype('f')
 
         self.embed = TimeEmbedding(embed_W)
         self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)
+        # TimeAttensionレイヤ追加 ch07/seq2seq.pyとの違い
         self.attention = TimeAttention()
         self.affine = TimeAffine(affine_W, affine_b)
+        # TimeLSTMレイヤとAffineの間に、TimeAttensionレイヤを追加
         layers = [self.embed, self.lstm, self.attention, self.affine]
 
         self.params, self.grads = [], []
@@ -42,26 +56,39 @@ class AttentionDecoder:
             self.grads += layer.grads
 
     def forward(self, xs, enc_hs):
-        h = enc_hs[:,-1]
+        h = enc_hs[:, -1]
         self.lstm.set_state(h)
 
         out = self.embed.forward(xs)
         dec_hs = self.lstm.forward(out)
         c = self.attention.forward(enc_hs, dec_hs)
+        # 図8-21参照
+        # Affineレイヤの入力は下記の2つレイヤの結果なので、concatenateで行列を結合
+        # c: TimeAttensionの結果
+        # dec_hs: TimeLSTMの結果
         out = np.concatenate((c, dec_hs), axis=2)
         score = self.affine.forward(out)
 
         return score
 
     def backward(self, dscore):
+        # dout: affine -> TimeAttension and TimeLSTM
         dout = self.affine.backward(dscore)
         N, T, H2 = dout.shape
         H = H2 // 2
 
-        dc, ddec_hs0 = dout[:,:,:H], dout[:,:,H:]
+        # dc: TimeAffine -> TimeAttension
+        # ddec_hs0: -> TimeAffine -> Time LSTM
+        dc, ddec_hs0 = dout[:, :, :H], dout[:, :, H:]
+        # denc_hs: TimeAttension -> Encoder
+        # ddec_hs1: TimeAttension -> TimeLSTM
         denc_hs, ddec_hs1 = self.attention.backward(dc)
+        # TimeAffine and TimeAttension -> TimeLSTM
+        # 下手に展開しない方がわかりやすいかも
+        # dout = self.lstm.backward(ddec_hs0 + ddec_hs1)
         ddec_hs = ddec_hs0 + ddec_hs1
         dout = self.lstm.backward(ddec_hs)
+
         dh = self.lstm.dh
         denc_hs[:, -1] += dh
         self.embed.backward(dout)
@@ -69,6 +96,10 @@ class AttentionDecoder:
         return denc_hs
 
     def generate(self, enc_hs, start_id, sample_size):
+        '''
+        文章の生成。ch07/seq2seq.generate()との違いは、
+        TimeAttensionレイヤが加わっただけ。
+        '''
         sampled = []
         sample_id = start_id
         h = enc_hs[:, -1]
@@ -92,9 +123,13 @@ class AttentionDecoder:
 class AttentionSeq2seq(Seq2seq):
     def __init__(self, vocab_size, wordvec_size, hidden_size):
         args = vocab_size, wordvec_size, hidden_size
+        # EncoderではなくAttentionEncoderを使用
         self.encoder = AttentionEncoder(*args)
+        # DecoderではなくAttensionDecoderを使用
         self.decoder = AttentionDecoder(*args)
         self.softmax = TimeSoftmaxWithLoss()
 
         self.params = self.encoder.params + self.decoder.params
         self.grads = self.encoder.grads + self.decoder.grads
+
+    # forward(), backward(), generate()はSeq2seqクラスを継承
